cde155d5aad00bb09e711c870a032b39b128327f
Zero <jgm262@naver.com>
2024-03-25 17:25:11 +0900

    git init 2

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler'; // swipe 등을 이용한 제스쳐로 기능 수행
 import 'react-native-devsettings';
 import {NavigationContainer} from '@react-navigation/native';
-import Router from './Router';
+import Router from './src/Router';
 
 function App(): React.JSX.Element {
   return (
